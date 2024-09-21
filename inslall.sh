d176f34db1739b2c5eef20763b930ea43c246dfb
Mohammed Boukhal <85312140+Mboukhal@users.noreply.github.com>
2024-09-21 13:02:00 +0100

    up

--- inslall.sh
+++ inslall.sh
@@ -3,3 +3,4 @@ sudo apt-get install python3-bluez
 sudo apt-get install python3-dev
 sudo apt-get install libbluetooth-dev
 sudo apt install python3.12-venv
+sudo apt-get install libbluetooth-dev python3-pip libglib2.0-dev
